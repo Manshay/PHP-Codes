2220c8f38709248eb133839c8df9d307ab61bda7
Manshay <manshaybhandari@gmail.com>
2025-02-08 17:24:28 +0530

    files added

---
+++ code17.php
@@ -0,0 +1,10 @@
+<?php
+
+    	define("GREETING", "hello world!",true);
+    	echo GREETING;
+    	echo "<br>";
+    	echo GREETING;
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)";
+
+?>
---
+++ code9.php
@@ -0,0 +1,9 @@
+<?php
+
+    	// Open a file for reading
+    	$handle = fopen("note.txt", "r");
+    	var_dump($handle);
+	
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)"; 
+?>
