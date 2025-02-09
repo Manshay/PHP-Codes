a603494f5e422f1f3e496d13a4b9522024cbdd5d
Manshay <manshaybhandari@gmail.com>
2025-02-09 23:29:39 +0530

    files added

---
+++ code35.php
@@ -0,0 +1,12 @@
+<?php
+	function incrementValue(&$num) {
+    		$num += 10; // Increment the value by 10
+	}
+
+	$value = 20;
+	incrementValue($value);
+
+	echo "The updated value is: " . $value; 
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)";
+?>
---
+++ code37.php
@@ -0,0 +1,11 @@
+<?php
+	$numbers = [10, 20, 30, 40, 50];
+	foreach ($numbers as $num) echo $num . "<br>";
+
+	$numbers[] = 60;
+	echo "After adding: <br>";
+
+	foreach ($numbers as $num) echo $num . "<br>";
+	echo "<br><br>";
+	echo "Code executed by Manshay(0221BCA106)";
+?>
