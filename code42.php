c89935d527e3da39ae858cafca8d66d973019b38
Manshay <manshaybhandari@gmail.com>
2025-04-05 14:33:45 +0530

    files added

--- code42.php
+++ code42.php
@@ -1,18 +1,18 @@
 <?php
-$students = [
-    "Manshay" => ["C" => 85, "Algo" => 90, "DS" => 78, "DBMS" => 88, "Stats" => 76],
-    "Krish" => ["C" => 92, "Algo" => 80, "DS" => 89, "DBMS" => 84, "Stats" => 90],
-    "Harsh" => ["C" => 75, "Algo" => 85, "DS" => 80, "DBMS" => 79, "Stats" => 82]
-];
+// Create an associative array
+    $studentMarks = array(
+        "Math" => 85,
+        "English" => 78,
+        "Science" => 92,
+        "History" => 74,
+        "Computer" => 88
+    );
 
-// Printing the marks
-foreach ($students as $name => $subjects) {
-    echo "Marks for $name:<br>";
-    foreach ($subjects as $subject => $mark) {
+// Print the marks
+    echo "Student Marks:<br>";
+    foreach ($studentMarks as $subject => $mark) {
         echo "$subject: $mark<br>";
     }
     echo "<br>";
-}
-
-echo "Code executed by Manshay(0221BCA106)"
+    echo "Code executed by Manshay(0221BCA106)";
 ?>
